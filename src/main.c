c9d2476be0bd43bdd0592ca145e272fe34b88cdf
Ragib Asif <ragibasif@tuta.io>
2025-09-22 15:13:30 -0400

    feat: server can handle multiple requests

--- src/main.c
+++ src/main.c
@@ -27,6 +27,11 @@
 #define BACKLOG         10 // number of pending connections in queue
 #define WWW_EXAMPLE_COM "https://www.example.com/"
 
+void h_sigchld_handler( int s ) {
+    (void)s; // quiet unused variable warning
+    while ( waitpid( -1, NULL, WNOHANG ) > 0 );
+}
+
 char *h_int_to_str( int n ) {
     // NOTE: snprintf is safer than sprintf and should always be used over
     // sprintf
@@ -39,6 +44,14 @@ char *h_int_to_str( int n ) {
     char *buffer = malloc( sizeof *buffer * ( size + 1 ) );
     snprintf( buffer, size + 1, "%d", n );
     return buffer;
+}
+
+// NOTE: get sockaddr, IPv4 or IPv6
+void *h_get_in_addr( struct sockaddr *sa ) {
+    if ( sa->sa_family == AF_INET ) {
+        return &( ( (struct sockaddr_in *)sa )->sin_addr );
+    }
+    return &( ( (struct sockaddr_in6 *)sa )->sin6_addr );
 }
 
 int main( int argc, char **argv ) {
@@ -76,32 +89,98 @@ int main( int argc, char **argv ) {
         exit( EXIT_FAILURE );
     }
 
-    // NOTE: get a socket file descriptor
-    int socket_fd =
-        socket( res->ai_family, res->ai_socktype, res->ai_protocol );
-
+    int socket_fd;
+    // NOTE: iterate through the res linked list and bind to the first valid
+    struct addrinfo *curr;
     // NOTE: to stop bind() from failing with a "Address already in use." error,
     // set an option to reuse addresses
-    bool reuse = true;
-    setsockopt( socket_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof reuse );
-    // NOTE: bind the socket to a port
-    bind( socket_fd, res->ai_addr, res->ai_addrlen );
+    int reuse = 1;
+    for ( curr = res; curr; curr = curr->ai_next ) {
+        // NOTE: get a socket file descriptor
+        if ( ( socket_fd = socket( res->ai_family, res->ai_socktype,
+                                   res->ai_protocol ) ) == -1 ) {
+            perror( "server: socket" );
+            continue;
+        }
+
+        if ( setsockopt( socket_fd, SOL_SOCKET, SO_REUSEADDR, &reuse,
+                         sizeof reuse ) == -1 ) {
+            perror( "setsockopt" );
+            exit( EXIT_FAILURE );
+        }
+
+        // NOTE: bind the socket to a port
+        if ( bind( socket_fd, res->ai_addr, res->ai_addrlen ) == -1 ) {
+            close( socket_fd );
+            perror( "server: bind" );
+            continue;
+        }
+        break;
+    }
+    // NOTE: must remember to free allocated memory
+    freeaddrinfo( res ); // cleanup res
+    res = NULL;
+
+    // NOTE: if curr reaches past the last node of the list, then server failed
+    // to bind
+    if ( !curr ) {
+        fprintf( stderr, "server: failed to bind\n" );
+        exit( EXIT_FAILURE );
+    }
+
     // NOTE: listen on the socket
-    listen( socket_fd, BACKLOG );
+    if ( listen( socket_fd, BACKLOG ) == -1 ) {
+        perror( "listen" );
+        exit( EXIT_FAILURE );
+    }
 
-    // NOTE: accept incoming connections
+    struct sigaction sa;
+    sa.sa_handler = h_sigchld_handler; // reap all dead processes
+    sigemptyset( &sa.sa_mask );
+    sa.sa_flags = SA_RESTART;
+    if ( sigaction( SIGCHLD, &sa, NULL ) == -1 ) {
+        perror( "sigaction" );
+        exit( EXIT_FAILURE );
+    }
+
+    printf( "server: waiting for connection...\n" );
+
+    char                    s[INET6_ADDRSTRLEN];
     struct sockaddr_storage client_addr;
-    socklen_t               client_addr_size = sizeof client_addr;
-    int                     client_fd =
-        accept( socket_fd, (struct sockaddr *)&client_addr, &client_addr_size );
+    socklen_t               client_addr_size;
+    int                     client_fd;
+    for ( ;; ) {
+        // NOTE: accept incoming connections
+        client_addr_size = sizeof client_addr;
+        client_fd        = accept( socket_fd, (struct sockaddr *)&client_addr,
+                                   &client_addr_size );
 
-    // NOTE: send() and recv() are blocking calls
-    // NOTE: recv() will block until data arrives
-    // NOTE: send() will block but its very rare
-    // NOTE: blocking == sleep
+        if ( client_fd == -1 ) {
+            perror( "accept" );
+            continue;
+        }
+
+        inet_ntop( client_addr.ss_family,
+                   h_get_in_addr( (struct sockaddr *)&client_addr ), s,
+                   sizeof s );
+        printf( "server: got connection from %s\n", s );
+
+        // NOTE: send() and recv() are blocking calls
+        // NOTE: recv() will block until data arrives
+        // NOTE: send() will block but its very rare
+        // NOTE: blocking == sleep
+        if ( !fork() ) {        // child process
+            close( socket_fd ); // child doesn't need the listener
+            if ( send( client_fd, "Hello, World!", 13, 0 ) == -1 ) {
+                perror( "send" );
+            }
+            close( client_fd );
+            exit( EXIT_SUCCESS );
+        }
+        close( client_fd ); // parent doesn't need this
+    }
 
     // NOTE: must remember to free allocated memory
-    freeaddrinfo( res );
     free( port );
     port = NULL;
 
